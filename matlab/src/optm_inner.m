375040997b51da2c4cdffe1b19f74a0ae69f728a
Éric Thiébaut <eric.thiebaut@univ-lyon1.fr>
2021-06-09 18:58:46 +0200

    Fix a regression (up to twice slower)
    Changing the definition of the inner product may strongly impact
    optimization methods (optm_conjgrad and optm_vmlmb)

--- matlab/src/optm_inner.m
+++ matlab/src/optm_inner.m
@@ -5,9 +5,9 @@
 %%
 %% If `x` and `y` are the same, it is more efficient to call `optm_norm2(x)^2`.
 function s = optm_inner(x, y)
-    s = x(:)'*y(:); % <-- the fastest when x and y are not the same
+    %%s = x(:)'*y(:); % <-- the fastest when x and y are not the same
     %%s = sum(x(:).*y(:));
     %%s = sum(bsxfun(@times, x(:), y(:)));
-    %%s = sum((x.*y)(:));
+    s = sum((x.*y)(:));
     %%s = sum(bsxfun(@times, x, y)(:));
 end
